bc0ac8a8ad50d13ca34e84bacaca91604b2bb5d1
Juan Lopes <juan@geminidesign.nl>
2017-05-19 11:38:09 +0200

    first commit

---
+++ js/controllers/results.js
@@ -0,0 +1,34 @@
+(function(){
+    angular
+        .module('turtleFacts')
+        .controller('resultsCtrl', ResultsController);
+
+        ResultsController.$inject = ['quizMetrics', 'DataService'];
+
+        function ResultsController(quizMetrics, DataService){
+            var vm = this;
+
+            vm.quizMetrics = quizMetrics;
+            vm.dataService = DataService;
+            vm.getAnswerClass = getAnswerClass;
+            vm.setActiveQuestion = setActiveQuestion;
+            vm.calculatePerc = calculatePerc;
+            vm.activeQuestion = 0;
+
+            function getAnswerClass(index){
+                if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
+                    return 'bg-success';
+                }else if(index === DataService.quizQuestions[vm.activeQuestion].selected){
+                    return 'bg-danger';
+                }
+            }
+
+            function setActiveQuestion(index){
+                vm.activeQuestion = index;
+            }
+
+            function calculatePerc(){
+                return (quizMetrics.numCorrect / DataService.quizQuestions.length) * 100;
+            }
+        }
+}());
